64a8af806d574e63e67d85038aa75fa6c9b2c573
Cody Cook <codycook33@gmail.com>
2018-10-28 21:32:08 -0700

    Added most of the sprites that I was working on And added basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float xInput;
+	public float speed;
+	public Rigidbody2D rbody;
+	public float jumpPower;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		xInput = Input.GetAxis( "Horizontal" );
+		if( Input.GetButtonDown( "Jump" ) ){
+			Jump();
+		}
+	}
+
+	void FixedUpdate() {
+		rbody.velocity = new Vector2( xInput * speed, rbody.velocity.y );
+	}
+
+	void Jump() {
+		rbody.AddForce( Vector2.up * jumpPower, ForceMode2D.Impulse );
+	}
+}
