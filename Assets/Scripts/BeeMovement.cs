be90b1e2d59075648095b15b2f0df6436d704f06
Cody Cook <codycook33@gmail.com>
2018-10-29 17:51:01 -0700

    Added more sprites, Bee movement, and a basic UI layout

---
+++ Assets/Scripts/BeeMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeMovement : MonoBehaviour {
+
+	private bool dirRight = true;
+    public float speed;
+ 
+    void Update () {
+        if (dirRight)
+            transform.Translate (Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate (-Vector2.right * speed * Time.deltaTime);
+        
+        if(transform.position.x >= 43.0f) {
+            Flip();
+            dirRight = false;
+        }
+        
+        if(transform.position.x <= -43) {
+            Flip();
+            dirRight = true;
+        }
+    }
+
+    void Flip()
+    {
+    	Vector3 theScale = transform.localScale;
+    	theScale.x *= -1;
+    	transform.localScale = theScale;
+    }
+    void OnTriggerEnter()
+    {
+    	//death screen and stop time
+    	
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject moth;
+	public float offset;
+	public float mothPosY;
+	public float camPosY;
+
+	// Use this for initialization
+	void Start () {
+		camPosY = transform.position.y;
+		mothPosY = moth.transform.position.y;
+		offset = camPosY - mothPosY;
+
+	}
+	void Update()
+	{
+		camPosY = transform.position.y;
+		mothPosY = moth.transform.position.y;
+	}
+	// Update is called once per frame
+	void LateUpdate () {
+		camPosY = mothPosY + offset;
+		transform.position = new Vector3(10.1f, camPosY, -60f);
+	}
+}
